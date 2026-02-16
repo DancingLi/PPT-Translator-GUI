e67d32750a25f64f3a86e99135a7c303ae5c6c4a
DancingLi <148843299+DancingLi@users.noreply.github.com>
2026-02-16 08:02:45 +0300

    Delete gui directory

--- gui/widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-"""GUI widgets package."""
-
-from .file_selector import FileSelector
-from .provider_selector import ProviderSelector
-
-__all__ = [
-    'FileSelector',
-    'ProviderSelector',
-]
